f51127d490c9b37ea9bd498d527e74560b5bf461
Mohamed Asir <asir.abm@gmail.com>
2023-03-15 23:52:46 +0530

    finished

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 const express=require('express')
 const app=express()
-const user=[
+const bcrypt=require('bcrypt')
+app.use(express.json())
+const users=[
   
 ]
 
-app.get('/user',(req,res)=>{res.json(post)})
+app.get('/users',(req,res)=>{res.json(users)})
+app.post('/users',async(req,res)=>{
+   const hashpassword=await bcrypt.hash(req.body.password,10)
+
+    const user={
+    name:req.body.name,
+    password:hashpassword
+  }
+  users.push(user)
+  res.sendStatus(201).send()
+})
+
+app.post('/login',async(req,res)=>{
+    const name=req.body.name
+    const u1=users.find(user=>user.name==name)
+    console.log(u1)
+    if(u1 == null){
+        res.status(400).send('Cannot find user')
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            res.send('sucess')
+        }
+        else{
+            res.send('Wrong password')
+        }
+        
+    }
+    catch(e){
+        res.status(500).send()
+    }
+
+})
+
+
 
 app.listen(4000)
